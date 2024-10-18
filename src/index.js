ff732c05aeb1fb808b034510cec12d008806e0d8
Justin Nguyen <justinnguyen@justins-air.resource.campus.njit.edu>
2024-10-18 19:51:08 -0400

    Completed REST API microservice

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const port = 3000;
 // Middleware to parse JSON bodies
 app.use(express.json());
 
+const users = [];
+
 // **************************************************************
 // Put your implementation here
 // If necessary to add imports, please do so in the section above
@@ -12,6 +14,36 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+app.post('/users', (req, res) => {
+    const { name, email } = req.body;
+    const newUser = { id: users.length + 1, name, email };
+    users.push(newUser);
+    res.status(201).json(newUser);
+  });
+
+app.get('/users/:id', (req, res) => {
+    const user = users.find(u => u.id === parseInt(req.params.id));
+    if (!user) return res.status(404).send('User not found');
+    res.json(user);
+  });
+
+  app.put('/users/:id', (req, res) => {
+    const user = users.find(u => u.id === parseInt(req.params.id));
+    if (!user) return res.status(404).send('User not found');
+    const { name, email } = req.body;
+    user.name = name;
+    user.email = email;
+    res.json(user);
+  });
+
+  app.delete('/users/:id', (req, res) => {
+    const userIndex = users.findIndex(u => u.id === parseInt(req.params.id));
+    if (userIndex === -1) return res.status(404).send('User not found');
+    users.splice(userIndex, 1);
+    res.status(204).send();
+  });
+  
 
 // Do not touch the code below this comment
 // **************************************************************
